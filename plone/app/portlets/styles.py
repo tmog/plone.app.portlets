0ed7d6843f6eb0ba9f3798b98b676db6ecfef1a2
Kai Lautaportti <kai.lautaportti@hexagonit.fi>
2012-01-07 22:20:46 +0200

    Put all style related implementation in styles.py (for now).

--- plone/app/portlets/styles.py
+++ plone/app/portlets/styles.py
@@ -1,8 +1,19 @@
+# -*- coding: utf-8 -*-
+from plone.app.portlets import PloneMessageFactory as _
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
 from zope.interface import Invalid
+from zope.interface import implements
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+import logging
 import re
 
+logger = logging.getLogger(__name__)
+RE_CSS_CLASS = re.compile('^-?[_a-zA-Z][_a-zA-Z0-9-]+$')
 
-# Styles formatting validator for control panel config
+
 class InvalidCssClassError(ValueError):
     """An exception thrown when a style does not have a valid CSS class."""
 
@@ -76,28 +87,53 @@ def _is_valid_css_class(string):
 
     :rtype: boolean
     :return: True if string is a valid CSS class, False otherwise
-
-    >>> from hexagonit.portletstyle import _is_valid_css_class
-
-    Valid classes
-    >>> _is_valid_css_class('foo')
-    True
-    >>> _is_valid_css_class('-single-dash')
-    True
-    >>> _is_valid_css_class('_underscore')
-    True
-    >>> _is_valid_css_class('numbered123')
-    True
-
-    Invalid classes
-    >>> _is_valid_css_class('x')
-    False
-    >>> _is_valid_css_class('--double-dash')
-    False
-    >>> _is_valid_css_class('123leading-numbers')
-    False
     """
-    exp = re.compile('^-?[_a-zA-Z][_a-zA-Z0-9-]+$')
-    if exp.match(string):
-        return True
-    return False
+    return RE_CSS_CLASS.match(string) is not None
+
+
+class StylesVocabulary(object):
+    """Vocabulary factory of portlet styles read from control panel registry."""
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        registry = getUtility(IRegistry)
+        try:
+            styles = registry['plone.app.portlets.browser.interfaces.IPortletStyles.portlet_styles']
+        except KeyError:
+            # if portlet_styles field is not found in plone.app.registry, just
+            # return an empty list -> probably the product is registered (zcml)
+            # but not installed (GenericSetup)
+            styles = []
+
+        # always have the default "default style" option available, which is effectively a no-op
+        terms = [SimpleTerm(title=_(u"Default style"), value=" ")]
+
+        # add styles from the control panel, but filter out invalid ones
+        for style in styles:
+
+            try:
+                css, title = parse_style(style)
+            except EmptyLineError:
+                continue
+            except InvalidCssClassError:
+                logger.warn("Filtered out a style because it doesn't have a valid CSS class: '%s'" % style)
+                continue
+            except:
+                logger.warn("Filtered out a style because it cannot be parsed: '%s'" % style)
+                continue
+
+            terms.append(SimpleTerm(title=title, value=css))
+
+        # if a portlet has a style assigned that is no longer listed in
+        # portlet_styles, than we need to add it to the drop-down menu,
+        # so it's still possible to select it
+        if hasattr(context, 'portlet_style') and not context.portlet_style in [t.value for t in terms]:
+            terms.append(SimpleTerm(
+                    title=context.portlet_style,
+                    value=context.portlet_style,
+                ))
+
+        return SimpleVocabulary(terms)
+
+
+StylesVocabularyFactory = StylesVocabulary()
